8d3c57307b9c8d3f849a9627eafe8cea55912f64
vaishnavicb <vaishnaviburhanpure@gmail.com>
2026-04-15 23:12:20 +0530

    Updated MongoDB Atlas connection

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
+
 
 connectDB();
 
@@ -23,6 +25,6 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "Backend is working", timestamp: new Date().toISOString() });
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
 );
